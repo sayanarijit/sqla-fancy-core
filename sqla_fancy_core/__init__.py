570c91e74c76defff1bf5367990278157d472c70
Arijit Basu <sayanarijit@gmail.com>
2023-02-27 11:56:40 +0530

    Some fixes and refactoring

--- sqla_fancy_core/__init__.py
+++ sqla_fancy_core/__init__.py
@@ -20,47 +20,47 @@ class TableFactory:
         self.defaults = dict(self.DEFAULTS, **(defaults or {}))
         self.fk_defaults = dict(self.FK_DEFAULTS, **(fk_defaults or {}))
 
-    def column(self, *args, **kwargs) -> sa.Column:
+    def col(self, *args, **kwargs) -> sa.Column:
         kwargs = dict(self.defaults, **kwargs)
         col = sa.Column(*args, **kwargs)
         self.c.append(col)
         return col
 
     def integer(self, name: str, *args, **kwargs) -> sa.Column:
-        return self.column(name, sa.Integer, *args, **kwargs)
+        return self.col(name, sa.Integer, *args, **kwargs)
 
     def string(self, name: str, *args, **kwargs) -> sa.Column:
-        return self.column(name, sa.String, *args, **kwargs)
+        return self.col(name, sa.String, *args, **kwargs)
 
     def text(self, name: str, *args, **kwargs) -> sa.Column:
-        return self.column(name, sa.Text, *args, **kwargs)
+        return self.col(name, sa.Text, *args, **kwargs)
 
     def float(self, name: str, *args, **kwargs) -> sa.Column:
-        return self.column(name, sa.Float, *args, **kwargs)
+        return self.col(name, sa.Float, *args, **kwargs)
 
     def numeric(self, name: str, *args, **kwargs) -> sa.Column:
-        return self.column(name, sa.Numeric, *args, **kwargs)
+        return self.col(name, sa.Numeric, *args, **kwargs)
 
     def bigint(self, name: str, *args, **kwargs) -> sa.Column:
-        return self.column(name, sa.BigInteger, *args, **kwargs)
+        return self.col(name, sa.BigInteger, *args, **kwargs)
 
     def smallint(self, name: str, *args, **kwargs) -> sa.Column:
-        return self.column(name, sa.SmallInteger, *args, **kwargs)
+        return self.col(name, sa.SmallInteger, *args, **kwargs)
 
     def timestamp(self, name: str, *args, **kwargs) -> sa.Column:
-        return self.column(name, sa.TIMESTAMP, *args, **kwargs)
+        return self.col(name, sa.TIMESTAMP, *args, **kwargs)
 
     def date(self, name: str, *args, **kwargs) -> sa.Column:
-        return self.column(name, sa.Date, *args, **kwargs)
+        return self.col(name, sa.Date, *args, **kwargs)
 
     def datetime(self, name: str, *args, **kwargs) -> sa.Column:
-        return self.column(name, sa.DateTime, *args, **kwargs)
+        return self.col(name, sa.DateTime, *args, **kwargs)
 
     def today(self, name: str, *args, **kwargs) -> sa.Column:
         return self.date(name, default=sa.func.now(), *args, **kwargs)
 
     def time(self, name: str, *args, **kwargs) -> sa.Column:
-        return self.column(name, sa.Time, *args, **kwargs)
+        return self.col(name, sa.Time, *args, **kwargs)
 
     def timenow(self, name: str, *args, **kwargs) -> sa.Column:
         return self.time(name, default=sa.func.now(), *args, **kwargs)
@@ -69,7 +69,7 @@ class TableFactory:
         return self.datetime(name, default=sa.func.now(), *args, **kwargs)
 
     def boolean(self, name: str, *args, **kwargs) -> sa.Column:
-        return self.column(name, sa.Boolean, *args, **kwargs)
+        return self.col(name, sa.Boolean, *args, **kwargs)
 
     def true(self, name: str, *args, **kwargs):
         return self.boolean(name, default=True, *args, **kwargs)
@@ -77,21 +77,18 @@ class TableFactory:
     def false(self, name: str, *args, **kwargs):
         return self.boolean(name, default=False, *args, **kwargs)
 
-    def uuid(self, name: str, *args, **kwargs) -> sa.Column:
-        return self.column(name, sa.UUID, *args, **kwargs)
-
     def foreign_key(self, name: str, ref: str | sa.Column, *args, **kwargs):
         fk = sa.ForeignKey(ref, **self.fk_defaults)
-        return self.column(name, fk, *args, **kwargs)
+        return self.col(name, fk, *args, **kwargs)
 
     def enum(self, name: str, enum: type, *args, **kwargs) -> sa.Column:
-        return self.column(name, sa.Enum(enum), *args, **kwargs)
+        return self.col(name, sa.Enum(enum), *args, **kwargs)
 
     def json(self, name: str, *args, **kwargs) -> sa.Column:
-        return self.column(name, sa.JSON, *args, **kwargs)
+        return self.col(name, sa.JSON, *args, **kwargs)
 
     def array(self, name: str, *args, **kwargs) -> sa.Column:
-        return self.column(name, sa.ARRAY, *args, **kwargs)
+        return self.col(name, sa.ARRAY, *args, **kwargs)
 
     def array_int(self, name: str, *args, **kwargs) -> sa.Column:
         return self.array(name, sa.Integer, *args, **kwargs)
@@ -129,25 +126,12 @@ class TableFactory:
     def array_boolean(self, name: str, *args, **kwargs) -> sa.Column:
         return self.array(name, sa.Boolean, *args, **kwargs)
 
-    def array_uuid(self, name: str, *args, **kwargs) -> sa.Column:
-        return self.array(name, sa.UUID, *args, **kwargs)
-
     def array_enum(self, name: str, enum: type, *args, **kwargs) -> sa.Column:
         return self.array(name, sa.Enum(enum), *args, **kwargs)
 
     def auto_id(self, name="id", *args, **kwargs) -> sa.Column:
         return self.integer(
             name, primary_key=True, index=True, autoincrement=True, *args, **kwargs
-        )
-
-    def auto_uuid(self, name="id", *args, **kwargs) -> sa.Column:
-        return self.uuid(
-            name,
-            primary_key=True,
-            index=True,
-            default=sa.func.uuid_generate_v4(),
-            *args,
-            **kwargs
         )
 
     def updated_at(self, name="updated_at", *args, **kwargs) -> sa.Column:
