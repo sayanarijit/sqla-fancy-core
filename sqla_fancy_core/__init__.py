0da06056b0ff3e3bd197213ece74053cc574a5d0
Arijit Basu <sayanarijit@gmail.com>
2025-11-14 14:29:11 +0530

    Allow direct import of @connect decorator
    ```
    from sqla_fancy_core import connect, transact, Inject
    ```

--- sqla_fancy_core/__init__.py
+++ sqla_fancy_core/__init__.py
@@ -1,11 +1,5 @@
 """SQLAlchemy core, but fancier."""
 
-"""SQLAlchemy core, but fancier."""
-
 from sqla_fancy_core.builders import TableBuilder  # noqa
-from sqla_fancy_core.wrappers import (  # noqa
-    FancyEngineWrapper,
-    AsyncFancyEngineWrapper,
-    fancy,
-)
-from sqla_fancy_core.decorators import transact, Inject  # noqa
+from sqla_fancy_core.wrappers import fancy, AsyncFancyEngineWrapper, FancyEngineWrapper  # noqa
+from sqla_fancy_core.decorators import transact, connect, Inject  # noqa
